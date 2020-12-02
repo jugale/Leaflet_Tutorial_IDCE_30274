e28bf3c4f7adf087987b4395c2cb6a901e4b9181
jugale <70239489+jugale@users.noreply.github.com>
2020-12-02 13:53:30 -0500

    Add files via upload

---
+++ selectTrees.js
@@ -0,0 +1,171 @@
+// Unique Tree GeoJSON feature
+ var uniquetrees =[{                                   //set variable
+       "type":"Feature",                               //list data type
+       "properties":{                                  //properties hold data associated with point
+         "Common_Nam":"Eastern Redcedar",
+         "Scientific":"Juniperus virginiana",
+         "Hazard":"None",
+         "Comments":"needs loping at base"
+       },                                               //close properties
+       "geometry":{                                     // geometry of feature
+         "type":"Point",                                // what type of geometry the feature has
+         "coordinates":[-71.831775279541574,42.258511570448206]   //Coordinates of point
+       }                                                // close geometry
+     },{                                               //close first point, open next; repeat format for each unique point
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Pin Oak",
+         "Scientific":"Quercus palustris",
+         "Hazard":"None",
+         "Comments":"needs loping at base"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831737810381654,42.259401340376378]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Horse Chestnut",
+         "Scientific":"Aesculus hippocastanum",
+         "Identifica":"DCR",
+         "Hazard":"None",
+         "Comments":"Hole in root collar, hollow, has species label"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831400000096522,42.257979999986347]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Katsura Tree",
+         "Scientific":"Cercidiphyllum japonicum",
+         "Hazard":"big hazard",
+         "Comments":"dead on western side"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831493840350163,42.257977969906278]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Japanese Umbrella Pine",
+         "Scientific":"Sciadopitys verticillata",
+         "Hazard":"dying",
+         "Comments":"tree leans, vines"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831558289664585,42.257823779558727]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"American Sweetgum",
+         "Scientific":"Liquidambar styraciflua",
+         "Hazard":"None",
+         "Comments":"vines, needs loping at base"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831545669705108,42.257269759669803]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Oriental Spruce",
+         "Scientific":"Picea orientalis",
+         "Hazard":"dead branches",
+         "Comments":"maple shoot"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831371909889981,42.257392609950905]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Sycamore Maple",
+         "Scientific":"Acer pseudoplatanus",
+         "Hazard":"None",
+         "Comments":" "
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831332649601748,42.257472649575526]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Sassafrass",
+         "Scientific":"Sassafras albidum",
+         "Hazard":"None",
+         "Comments":" "
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831313789551373,42.257503039755058]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Balsam Fir",
+         "Scientific":"Abies balsamea",
+         "Hazard":"None",
+         "Comments":"needs loping at base, insect damage"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831500000246479,42.257439999554542]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Alternate Leaf  Dogwood",
+         "Scientific":"Cornus alternifolia",
+         "Hazard":"None",
+         "Comments":" "
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.832789199431588,42.258000689949583]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Bigtooth Aspen",
+         "Scientific":"Populus grandidentata",
+         "Hazard":"dead branches",
+         "Comments":"has tag, insect damage on leaves"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.832741880305321,42.2581301999234]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Red Spruce",
+         "Scientific":"Picea rubens",
+         "Hazard":" ",
+         "Comments":" "
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.830522230541916,42.258114510203875]
+       }
+     },{
+       "type":"Feature",
+       "properties":{
+         "Common_Nam":"Butternut",
+         "Scientific":"Juglans cinerea",
+         "Hazard":"dead branches",
+         "Comments":"metal tag 23"
+       },
+       "geometry":{
+         "type":"Point",
+         "coordinates":[-71.831070579844607,42.258680850425463]
+       }
+     }];
